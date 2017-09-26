44bd5addfc6d0c5e753c3164457cba9a2cb11c6c
Andrii Los <puha212@gmail.com>
2017-09-26 19:27:33 +0200

    - WIP migration. Almost done :)

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -5,6 +5,7 @@ const rewireResolve = require('./rewires/resolve')
 module.exports = function override(config, env) {
   rewireResolve(config)
   rewireEslint(config)
+  config.module.rules[1].oneOf[0].options.limit = 1 //To stop img inlining (for Twitter etc integraions)
   rewireSc(config, env, {
     displayName: true,
   })
